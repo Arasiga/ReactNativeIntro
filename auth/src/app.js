14e8f58b3245d16bc679b418333ced3e3cccc6ad
Arasiga <alex.rasiga@gmail.com>
2019-06-03 11:03:59 -0700

    New auth app

---
+++ auth/src/app.js
@@ -0,0 +1,14 @@
+import React, { Copmponent } from 'react'
+import { View, Text } from 'react-native';
+
+class App extends Component {
+  render() {
+    return (
+      <View>
+        <Text>App</Text>
+      </View>
+    )
+  }
+}
+
+export default App;
