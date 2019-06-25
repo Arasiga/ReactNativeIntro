bcef57ff1a52df3fb44fa0a26dfc64b97c7a4822
Arasiga <alex.rasiga@gmail.com>
2019-06-25 10:50:43 -0700

    Finalized auth app

--- auth/src/app.js
+++ auth/src/app.js
@@ -1,10 +1,12 @@
 import React, { Component } from 'react'
 import { View } from 'react-native';
 import firebase from 'firebase';
-import { Header } from "./components/common";
+import { Header, Button, CardSection, Spinner } from "./components/common";
 import LoginForm from './components/LoginForm';
 
 class App extends Component {
+  state = { loggedIn: null };
+
   componentWillMount() {
     firebase.initializeApp({
       apiKey: 'AIzaSyDi-vo27x--KBaZkHVcRgfB0pRI6ctHZcE',
@@ -15,13 +17,38 @@ class App extends Component {
       messagingSenderId: '861969264385',
       appId: '1:861969264385:web:aa987e6cceccf31b'
     });
+
+    firebase.auth().onAuthStateChanged((user) => {
+      if (user) {
+        this.setState({ loggedIn: true });
+      } else {
+        this.setState({ loggedIn: false });
+      }
+    });
+  }
+
+  renderContent = () => {
+    switch(this.state.loggedIn) {
+      case true:
+        return (
+          <CardSection>
+            <Button onPress={() => firebase.auth().signOut()}>
+              Log Out
+            </Button>
+          </CardSection>
+        );
+      case false:
+        return <LoginForm />;
+      default:
+        return <Spinner size="large" />
+    }
   }
 
   render() {
     return (
       <View>
         <Header headerText='Authentication' />
-        <LoginForm />
+        {this.renderContent()}
       </View>
     )
   }
