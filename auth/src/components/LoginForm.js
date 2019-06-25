1007875b031e84e0d810541602451ed83899c0f1
Arasiga <alex.rasiga@gmail.com>
2019-06-25 10:27:10 -0700

    Adding auth login and spinner

--- auth/src/components/LoginForm.js
+++ auth/src/components/LoginForm.js
@@ -1,10 +1,63 @@
 import React, { Component } from 'react';
-import { Button, Card, CardSection, Input } from './common';
+import { Text } from 'react-native';
+import firebase from 'firebase';
+import { Button, Card, CardSection, Input, Spinner } from './common';
 
 class LoginForm extends Component {
-  state = { email: '', password: '' };
+  state = {
+    email: '',
+    password: '',
+    error: '',
+    loading: false
+  };
+
+  onButtonPress = () => {
+    const { email, password } = this.state;
+    const { onLoginSuccess, onLoginFail } = this;
+
+    this.setState({ error: '', loading: true });
+
+    firebase.auth().signInWithEmailAndPassword(email, password)
+      .then(onLoginSuccess)
+      .catch(() => {
+        firebase.auth().createUserWithEmailAndPassword(email, password)
+          .then(onLoginSuccess)
+          .catch(onLoginFail);
+      });
+  }
+
+  onLoginFail = () => {
+    this.setState({
+      error: 'Authentication Failed',
+      loading: false
+    });
+  }
+
+  onLoginSuccess = () => {
+    this.setState({
+      email: '',
+      password: '',
+      error: '',
+      loading: false
+    });
+  }
+
+  renderButton = () => {
+    if (this.state.loading) {
+      return <Spinner size="small" />;
+    }
+
+    return (
+      <Button onPress={this.onButtonPress}>
+        Log In
+      </Button>
+    );
+  }
 
   render() {
+    const { onButtonPress, renderButton } = this;
+    const { errorTextStyle } = styles;
+
     return (
       <Card>
         <CardSection>
@@ -26,13 +79,23 @@ class LoginForm extends Component {
           />
         </CardSection>
 
+        <Text style={errorTextStyle}>
+          {this.state.error}
+        </Text>
+
         <CardSection>
-          <Button>
-            Log In
-          </Button>
+          {renderButton()}
         </CardSection>
       </Card>
     );
+  }
+}
+
+const styles = {
+  errorTextStyle: {
+    fontSize: 20,
+    alignSelf: 'center',
+    color: 'red'
   }
 }
 
