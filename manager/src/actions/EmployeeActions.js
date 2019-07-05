37b0ea899e6b6ac80b06756671cbfd7a177dac14
Arasiga <alex.rasiga@gmail.com>
2019-07-05 11:07:12 -0700

    WIP: adding employee lists

--- manager/src/actions/EmployeeActions.js
+++ manager/src/actions/EmployeeActions.js
@@ -1,5 +1,9 @@
 import firebase from 'firebase';
-import { EMPLOYEE_UPDATE, EMPLOYEE_CREATE } from './types';
+import {
+  EMPLOYEE_UPDATE,
+  EMPLOYEE_CREATE,
+  EMPLOYEE_FETCH_SUCCESS
+} from './types';
 import { Actions } from 'react-native-router-flux';
 
 export const employeeUpdate = ({ prop, value }) => {
@@ -20,4 +24,15 @@ export const employeeCreate = ({ name, phone, shift }) => {
         Actions.pop();
       });
   }
+};
+
+export const employeeFetch = () => {
+  const { currentUser } = firebase.auth();
+
+  return (dispatch) => {
+    firebase.database().ref(`/users/${currentUser.uid}/employees`)
+      .on('value', snapshot => {
+        dispatch({ type: EMPLOYEE_FETCH_SUCCESS, payload: snapshot.val() });
+      });
+  };
 };
